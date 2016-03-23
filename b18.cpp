296f0d1afc3b150994c25f514676f9a3adafd05b
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-03-23 14:23:37 -0600

    FINISHED?

--- b18.cpp
+++ b18.cpp
@@ -1,3 +1,11 @@
+/*
+Name:
+Author:
+Class:
+Professor: Benjamin Kaiser
+Due Date:
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -9,60 +17,62 @@
 using namespace std;
 
 bool nand(bool in1, bool in2);
-int mapBlackToRed(int blackNum);
-int* mapRedToBlack(int redNum);
-int getCol(int blackNum);
-int getRow(int redNum);
-int* map2Darray(int redNum);
-
 
 //compile-time parameters
-/*
+//default parameters
 const int J = 8;
 const int K = 4;
 const int M = 10;
 const int N = 10;
-*/
+
+/*
 const int J = 4;
 const int K = 2;
 const int M = 3;
 const int N = 2;
+*/
 
 const int LENGTH = pow(2, J);
-
-struct wireDict
-{
-	int wire = -1;
-	bool sigval = false;
-};
-
+/*
+Name:
+Purpose:
+Author:
+*/
 int main(int argc, char** argv)
 {
 
 	int wirelist[(2 * M * N) + K];
-	bool redOut[(M * N)]; //my output from each gate
+	bool redOut[(M * N) + J]; //my output from each gate
 	bool blackIn[(2 * M * N)];
 
-	string truthTable[LENGTH]; //intial inputs iterated through
+	vector<string> truthTable(LENGTH); //intial inputs iterated through
 
 	bool jUsed[J];
-	int jCols[J];
-	
-//	wireDict jWires[J];
+	bool kUsed[K];
 
-	bool jVal[J];
-	bool kVal[K];
+	int jCols[J];
+	int kCols[K];
 
 	int tempRed, tempBlack;
 	int actualUsed = 0;
 
+	int kActualUsed = 0;
+
 	ifstream fin;
 	fin.open(argv[1]);
 
+	//kUsed indices
+
 	//initialize jCols to -1
-	for (int i = 0; i < J; i++)	
+	for (int i = 0; i < J; i++)
 	{
 		jCols[i] = -1;
+	}
+
+	//initialize kCols to -1
+	for (int i = 0; i < K; i++)
+	{
+		kCols[i] = -1;
 	}
 
 	// initialize wirelist to all -1 (unused)
@@ -75,6 +85,11 @@ int main(int argc, char** argv)
 	for (int i = 0; i < J; i++)
 	{
 		jUsed[i] = false;
+	}
+	//initialize kUsed to false
+	for (int i = 0; i < K; i++)
+	{
+		kUsed[i] = false;
 	}
 	//initialize blackIn to false
 	for (int i = 0; i < (2 * M * N); i++)
@@ -92,7 +107,13 @@ int main(int argc, char** argv)
 		{
 			jUsed[tempRed] = true;
 		}
+		if (tempBlack >= (2 * M * N))
+		{
+			kUsed[tempBlack - (2 * M * N)] = true;
+		}
 	}
+	//close wiring list
+	fin.close();
 
 	//COUNT THE NUMBER OF J'S ACTUALLY USED!!!!!
 	for (int i = 0; i < J; i++)
@@ -105,107 +126,97 @@ int main(int argc, char** argv)
 		}
 	}
 
-	cout << actualUsed << endl;
+	index = 0;
+	//COUNT THE NUMBER OF K's ACTUALLY USED!
+	for (int i = 0; i < K; i++)
+	{
+		if (kUsed[i] == true)
+		{
+			kActualUsed++;
+			kCols[index] = i + (2 * M * N);
+			index++;
+		}
+	}
 
 	// fill the truth table (initial values)
-	for (int i = 0; i < pow(2, actualUsed); i++) 
+	for (int i = 0; i < pow(2, actualUsed); i++)
 	{
 		bitset<J> tempTable(i);
 		tempTable <<= (J - actualUsed);
 		truthTable[i] = tempTable.to_string();
-		cout << truthTable[i] << endl;
+		//cout << truthTable[i] << endl;
 	}
-
-	//ONE BIG ASS FOR LOOP HERE GODDAMMIT!
-
-	//some fucking way I need to get my original j's (truthTable[i]) into my black inputs
-	//Do I have this already???????????? NO I FUCKING DO NOT HAVE THIS
-	for (int i = 0; i < actualUsed; i++)//Do I fucking j loop or length loop.
-	//pretty sure it's 2 * M * N.  
+	//output
+	for (int i = 0; i < (actualUsed + kActualUsed) * 5; i++)
 	{
-			jVal[jCols[i]] = (truthTable[3][i] == '1');
+		cout << "-";
 	}
-	for (int i = 0; i < J; i++)
+	cout << endl;
+
+	for (int i = 0; i < actualUsed; i++)
 	{
-		cout << jVal[i] << " "; //output makes sense
+		cout << "I" << jCols[i] << " | ";
 	}
-
-	//ok need to figure out how to know which j to send the signal from truth table to
-
-	//NAND and store into array indexed by red (redOut)
-	for (int i = 0; i < (2 * M * N) + K; i +=2)
+	//cout << "kactual " << kActualUsed << endl;
+	for (int i = 0; i < kActualUsed; i++)
 	{
-		//if (wirelist[i] != -1 && wirelist[i+1] != -1)
-			//redOut[i/2] = nand();
+		cout << "O" << i << " | ";
 	}
+	cout << endl;
+	for (int i = 0; i < (actualUsed + kActualUsed) * 5; i++)
+	{
+		cout << "-";
+	}
+	cout << endl;
+	//end output
 
-	//alright, Ben.  Goal:
-
-
-
-	//close wiring list
-	fin.close();
+	for (int bigIndex = 0; bigIndex < pow(2, actualUsed); bigIndex++)
+	{
+		for (int i = 0; i < actualUsed; i++)
+		{
+			redOut[jCols[i]] = (truthTable[bigIndex][i] == '1'); // setting up the original jVals
+			//for inputs
+		}
+		for (int i = 0; i < (2 * M * N); i +=2)
+		{
+			if (wirelist[i] != -1)
+			{
+				blackIn[i] = (redOut[wirelist[i]] == true);
+				blackIn[i+1] = (redOut[wirelist[i+1]] == true);
+				//nand the two values together and save to redOut
+				redOut[i/2 + J] = nand(blackIn[i], blackIn[i+1]);
+			}
+		}
+		//final outputs
+		for (int i = (2 * M * N); i < (2 * M * N) + K; i++)
+		{
+			if (wirelist[i] != -1)
+			{
+				blackIn[i] = (redOut[wirelist[i]] == true);
+			}
+		}
+		//output to screen
+		for (int i = 0; i < actualUsed; i++)
+		{
+			cout << truthTable[bigIndex][i] << "  | ";
+		}
+		for (int i = (2 * M * N); i < (2 * M * N) + K; i++)
+		{
+			if (kUsed[i - (2 * M * N)] == true)
+			{
+				cout << blackIn[i] << "  | ";
+			}
+		}
+		cout << endl;
+	}
 	return 0;
 }
-
+/*
+Name:
+Purpose:
+Author:
+*/
 bool nand(bool in1, bool in2)
 {
 	return !(in1 && in2);
 }
-
-//Probably uncessary anymore
-/*
-int mapBlackToRed(int blackNum)
-{
-	if (blackNum % 2 ==1)
-	{
-		blackNum--;
-		blackNum = blackNum / 2;
-		return blackNum + J;
-	}
-	else
-	{
-		blackNum = blackNum / 2;
-		return blackNum + J;
-	}
-}
-*/
-/*
-int* mapRedToBlack(int redNum)
-{
-	static int blacks[2];
-	blacks[0] = (redNum - J) * 2;
-	blacks[1] = ((redNum - J) * 2) + 1;
-	return blacks;
-}
-*/
-/*
-int getCol(int blackNum)
-{
-	if (blackNum < (2 * ((M * N))))
-	{
-		return blackNum/(2*M);
-	}
-	return -1;
-}
-*/
-/*
-int getRow(int redNum)
-{
-	if (redNum - J >= 0)
-	{
-		return (redNum - J) % M;
-	}
-	return -1;
-}
-*/
-/*
-int* map2Darray(int redNum)
-{
-	static int coords[2];
-	coords[0] = getRow(redNum);
-	int* temp = mapRedToBlack(redNum);
-	coords[1] = getCol(temp[1]);
-	return coords;
-}
-*/
